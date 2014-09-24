7d0837f48f653ba454a5802905a04aeb515f07f5
j k <github@dmachine.org>
2014-09-23 21:05:14 -0400

    more mappings

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -5,12 +5,16 @@ import java.util.Collection;
 import org.magnum.mobilecloud.video.repository.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
+import retrofit.http.Query;
 
 @Controller
 public class VideoController {
@@ -41,4 +45,25 @@ public class VideoController {
 	@RequestMapping(value=VIDEO_SVC_PATH+ "/{id}",method=RequestMethod.GET)
 	public @ResponseBody Video getVideoById(@PathVariable("id") long id) {
 	}
+	
+	@RequestMapping(value=VIDEO_TITLE_SEARCH_PATH,method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> findByTitle(@Param(TITLE_PARAMETER) String title) {
+	}
+	
+	@RequestMapping(value=VIDEO_DURATION_SEARCH_PATH,method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> findByDurationLessThan(@Param(DURATION_PARAMETER) long duration){
+	}
+	
+	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/like",method=RequestMethod.POST)
+	public Void likeVideo(@PathVariable("id") long id){
+	}
+	
+	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/unlike",method=RequestMethod.POST)
+	public Void unlikeVideo(@PathVariable("id") long id){
+	}
+
+	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/likedby",method=RequestMethod.GET)
+	public @ResponseBody Collection<String> getUsersWhoLikedVideo(@PathVariable("id") long id){
+	}
+
 }
