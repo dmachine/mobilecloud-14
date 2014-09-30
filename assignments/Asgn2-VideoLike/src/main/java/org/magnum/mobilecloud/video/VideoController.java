bef60554588737918c51c049819aa1630f575114
j k <github@dmachine.org>
2014-09-29 23:24:28 -0400

    passing tests for assignment 2

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -1,20 +1,28 @@
 package org.magnum.mobilecloud.video;
 
+import java.security.Principal;
 import java.util.Collection;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.mobilecloud.video.repository.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import retrofit.http.GET;
-import retrofit.http.POST;
-import retrofit.http.Path;
-import retrofit.http.Query;
+//import retrofit.http.GET;
+//import retrofit.http.POST;
+//import retrofit.http.Path;
+//import retrofit.http.Query;
+
+import com.google.common.collect.Lists;
 
 @Controller
 public class VideoController {
@@ -33,37 +41,85 @@ public class VideoController {
 	// The path to search videos by title
 	public static final String VIDEO_DURATION_SEARCH_PATH = VIDEO_SVC_PATH + "/search/findByDurationLessThan";
 	
+	@Autowired
+	private VideoRepository videos;
+	
 	@RequestMapping(value=VIDEO_SVC_PATH,method=RequestMethod.POST)
 	public @ResponseBody Video addVideo(@RequestBody Video video) {
+		video.setLikes(0);
+		videos.save(video);
 		return video;
 	}
 
 	@RequestMapping(value=VIDEO_SVC_PATH,method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList() {
+		return Lists.newArrayList(videos.findAll());
 	}
 
 	@RequestMapping(value=VIDEO_SVC_PATH+ "/{id}",method=RequestMethod.GET)
 	public @ResponseBody Video getVideoById(@PathVariable("id") long id) {
+		return videos.findOne(id);
 	}
 	
 	@RequestMapping(value=VIDEO_TITLE_SEARCH_PATH,method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> findByTitle(@Param(TITLE_PARAMETER) String title) {
+		return videos.findByName(title);
 	}
 	
 	@RequestMapping(value=VIDEO_DURATION_SEARCH_PATH,method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> findByDurationLessThan(@Param(DURATION_PARAMETER) long duration){
+		return videos.findByDurationLessThan(duration);
 	}
 	
 	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/like",method=RequestMethod.POST)
-	public Void likeVideo(@PathVariable("id") long id){
+	public void likeVideo(@PathVariable("id") long id, Principal p, HttpServletResponse response){
+		Video v = videos.findOne(id);
+		String username = p.getName();
+		System.out.println("Test");
+		if(v == null){
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+			return;
+		}
+		Set<String> likesUsernames = v.getLikesUsernames();
+		if(likesUsernames.contains(username)) {
+			response.setStatus(HttpStatus.BAD_REQUEST.value());
+			return;
+		}
+		likesUsernames.add(username);
+		v.setLikesUsernames(likesUsernames);
+		v.setLikes(likesUsernames.size());
+		videos.save(v);
+		return;
 	}
 	
 	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/unlike",method=RequestMethod.POST)
-	public Void unlikeVideo(@PathVariable("id") long id){
+	public void unlikeVideo(@PathVariable("id") long id, Principal p, HttpServletResponse response){
+		Video v = videos.findOne(id);
+		String username = p.getName();
+		if(v == null){
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+			return;
+		}
+		Set<String> likesUsernames = v.getLikesUsernames();
+		if(likesUsernames.contains(username)) {
+			likesUsernames.remove(username);
+			v.setLikesUsernames(likesUsernames);
+			v.setLikes(likesUsernames.size());
+			videos.save(v);
+			return;
+		} else {
+			response.setStatus(HttpStatus.BAD_REQUEST.value());
+			return;
+		}
 	}
 
 	@RequestMapping(value=VIDEO_SVC_PATH + "/{id}/likedby",method=RequestMethod.GET)
-	public @ResponseBody Collection<String> getUsersWhoLikedVideo(@PathVariable("id") long id){
+	public @ResponseBody Collection<String> getUsersWhoLikedVideo(@PathVariable("id") long id, HttpServletResponse response){
+		Video v = videos.findOne(id);
+		if(v == null){
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+		}
+		return v.getLikesUsernames();
 	}
 
 }
--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
@@ -3,7 +3,9 @@ package org.magnum.mobilecloud.video.repository;
 import java.util.Collection;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * An interface for a repository that can store Video
@@ -17,5 +19,5 @@ public interface VideoRepository extends CrudRepository<Video, Long>{
 
 	// Find all videos with a matching title (e.g., Video.name)
 	public Collection<Video> findByName(String title);
-	
+	public Collection<Video> findByDurationLessThan(long duration);
 }
